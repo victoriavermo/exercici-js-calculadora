ae481311d7c25b8d1898719076a4ebabff2bc26d
victoriavermo <victoriavermo@gmail.com>
2021-05-10 10:29:44 +0200

    Level 1

---
+++ js/calculadoraJS.js
@@ -0,0 +1,39 @@
+/*Operands:
+    1 -> +
+    2 -> -
+    3 -> *
+    4 -> /
+*/
+
+var num1;
+var num2;
+var operand; 
+var error = false;
+
+function calculadora(operand,num1,num2){
+
+    var result;
+
+    switch (operand){
+        case 'add':
+            result = num1+num2;
+            break;
+        case 'substract': 
+            result = num1-num2;   
+            break;
+        case 'multiply': 
+            result = num1*num2;
+            break;
+        case 'divide':
+            error = num2 != 0 ? false : true;
+            result = !error ? num1/num2 : null;
+
+            break;     
+    }
+
+    if(!error){
+      console.log(num1+' - '+num2+' = '+result);  
+    }
+    
+    
+}
